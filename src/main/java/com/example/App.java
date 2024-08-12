fc25f7185ab25ee51c50769069b6520b5f7a7303
Vinicius Caldato <133283560+vcaldato@users.noreply.github.com>
2024-08-12 15:01:14 -0300

    Update App.java
    Ajustes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,35 +6,30 @@ import java.sql.Statement;
 
 public class App {
     public static void main(String[] args) {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/testdb";
-        String username = "localhost";
+        String url = "jdbc:mysql://localhost:3306/testdb";
+        String user = "vini";
         String password = "9973";
 
         Connection connection = null;
         Statement statement = null;
 
         try {
-            // Conexão com o banco de dados
-            connection = DriverManager.getConnection(jdbcUrl, username, password);
-
-            // Criação do statement
+            connection = DriverManager.getConnection(url, user, password);
             statement = connection.createStatement();
-
-            // SQL para criar uma tabela
             String createTableSQL = "CREATE TABLE IF NOT EXISTS users ("
                                     + "id INT PRIMARY KEY AUTO_INCREMENT, "
                                     + "name VARCHAR(50), "
                                     + "email VARCHAR(50))";
-
-            // Executando o SQL
             statement.executeUpdate(createTableSQL);
 
-            System.out.println("Tabela criada com sucesso!");
+            String insertSQL = "INSERT INTO users (name, email) VALUES ('Vinicius Caldato', 'vinicaldato@gmail.com')";
+            statement.executeUpdate(insertSQL);
+            
+            System.out.println("Tabela criada com sucesso e registro inseridos com sucesso!");
 
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            // Fechando recursos
             try {
                 if (statement != null) statement.close();
                 if (connection != null) connection.close();
